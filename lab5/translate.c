7107d897967ef0908f4eea4a0c8e9b09c6265f32
derFischer <dingd2015@sjtu.edu.cn>
2018-11-26 01:25:24 +0800

    Update translate.c

--- lab5/translate.c
+++ lab5/translate.c
@@ -316,10 +316,14 @@ Tr_exp Tr_opExp(A_oper oper, Tr_exp left, Tr_exp right)
 		op = T_div;
 		break;
 	}
+	EM_error(0, "op exp here000\n");
+	T_exp leftt = unEx(left);
 	EM_error(0, "op exp here\n");
 	T_exp rightr = unEx(right);
 	EM_error(0, "op exp jere\n");
-	return Tr_Ex(T_Binop(op, unEx(left), rightr));
+	T_exp result = T_Binop(op, leftt, rightr);
+		EM_error(0, "op exp jeresdfasd\n");
+	return Tr_Ex(result);
 }
 
 Tr_exp Tr_intCompExp(A_oper op, Tr_exp left, Tr_exp right)
